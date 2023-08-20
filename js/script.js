6a3a79456a79c19eb75edea8a17ba9c259efacce
LyoLik1 <lyosha.neznakomov@gmail.com>
2023-08-20 19:14:30 +0300

    create: media rules for hotel section and do map with leaflet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// const map = document.querySelector('.map');
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
@@ -18,9 +20,28 @@ const swiper = new Swiper('.swiper', {
     enabled: true,
   },
   effect: 'fade',
-  // croosFade
 });
 
-addEventListener('keydown', function (e) {
-  console.log(e);
-});
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+    let map = L.map('map').setView([7.890980720197144, 98.294699280161189], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+      attribution:
+        '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([7.890980720197144, 98.294699280161189])
+      .addTo(map)
+      .bindPopup('DoubleTree by Hilton Phuket Banthai Resort')
+      .openPopup();
+  },
+  function () {
+    console.log('Impossible to get your location');
+  }
+);
