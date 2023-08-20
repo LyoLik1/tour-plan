d69a819de54c4e4eda27f10038c391944324b63e
LyoLik1 <lyosha.neznakomov@gmail.com>
2023-08-20 20:34:37 +0300

    fix: fix map for hosting

--- js/script.js
+++ js/script.js
@@ -22,29 +22,29 @@ const swiper = new Swiper('.swiper', {
   effect: 'fade',
 });
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
+// navigator.geolocation.getCurrentPosition(
+//   function (position) {
+//     const { latitude } = position.coords;
+//     const { longitude } = position.coords;
 
-    const coords = [latitude, longitude];
-    let map = L.map('map').setView([7.890980720197144, 98.294699280161189], 13);
+//     const coords = [latitude, longitude];
+//     let map = L.map('map').setView([7.890980720197144, 98.294699280161189], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-      maxZoom: 20,
-      attribution:
-        '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+//     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+//       maxZoom: 20,
+//       attribution:
+//         '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     }).addTo(map);
 
-    L.marker([7.890980720197144, 98.294699280161189])
-      .addTo(map)
-      .bindPopup('DoubleTree by Hilton Phuket Banthai Resort')
-      .openPopup();
-  },
-  function () {
-    console.log('Impossible to get your location');
-  }
-);
+//     L.marker([7.890980720197144, 98.294699280161189])
+//       .addTo(map)
+//       .bindPopup('DoubleTree by Hilton Phuket Banthai Resort')
+//       .openPopup();
+//   },
+//   function () {
+//     console.log('Impossible to get your location');
+//   }
+// );
 $(document).ready(function () {
   // Задаем скорость параллакса
   var parallaxSpeed = 0.3; // Экспериментируйте с этим значением
