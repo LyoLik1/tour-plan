fe4115c325f26a9e9a35f759922d0e96c36f7b5f
LyoLik1 <lyosha.neznakomov@gmail.com>
2023-08-22 20:58:24 +0300

    create: added review section & fix hotel slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// const map = document.querySelector('.map');
-
-const swiper = new Swiper('.swiper', {
+const hotelSwiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -11,62 +9,73 @@ const swiper = new Swiper('.swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
+    nextEl: '.hotel-slider-button__next',
+    prevEl: '.hotel-slider-button__prev',
+  },
+
+  // effect: 'cards',
+  // keyboard: {
+  //   enabled: true,
+  // },
+  effect: 'fade',
+});
+
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 
   // effect: 'cards',
   keyboard: {
     enabled: true,
   },
-  effect: 'fade',
+  // effect: 'fade',
 });
 
-// navigator.geolocation.getCurrentPosition(
-//   function (position) {
-//     const { latitude } = position.coords;
-//     const { longitude } = position.coords;
-
-//     const coords = [latitude, longitude];
-//     let map = L.map('map').setView([7.890980720197144, 98.294699280161189], 13);
-
-//     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-//       maxZoom: 20,
-//       attribution:
-//         '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     }).addTo(map);
-
-//     L.marker([7.890980720197144, 98.294699280161189])
-//       .addTo(map)
-//       .bindPopup('DoubleTree by Hilton Phuket Banthai Resort')
-//       .openPopup();
-//   },
-//   function () {
-//     console.log('Impossible to get your location');
-//   }
-// );
-$(document).ready(function () {
-  // Задаем скорость параллакса
-  var parallaxSpeed = 0.3; // Экспериментируйте с этим значением
-
-  // Получаем высоту окна браузера
-  var windowHeight = $(window).height();
-
-  // Получаем высоту изображения
-  var imageHeight = $('.parallax-bg').height();
-
-  // Получаем начальное значение прокрутки (снизу страницы)
-  var initialScroll = imageHeight - windowHeight;
-
-  // При прокрутке страницы
-  $(window).scroll(function () {
-    // Получаем значение прокрутки
-    var scrollTop = initialScroll - $(this).scrollTop();
-
-    // Рассчитываем позицию фона
-    var bgPosition = 'center ' + scrollTop * parallaxSpeed + 'px';
-
-    // Применяем позицию фона к элементу .parallax-bg
-    $('.parallax-bg').css('background-position', bgPosition);
-  });
+$('.parallax-window').parallax({
+  imageSrc: '../img/newsletter-bg.jpeg',
+  position: '-400px -400px',
 });
+
+// const slides = document.querySelectorAll('.reviews-slider__item');
+// const btnLeft = document.querySelector('.reviews-slider__button--prev');
+// const btnRight = document.querySelector('.reviews-slider__button--next');
+
+// console.log(slides);
+// currentSlide = 0;
+// const slidesNumber = slides.length - 1;
+
+// const moveToSlide = function (slide) {
+//   slides.forEach(
+//     (s, index) => (s.style.transform = `translateX(${(index - slide) * 100}%)`)
+//     //1 - -100%, 2 - 0%, 3 - 100%, 4 - 200%
+//   );
+// };
+
+// moveToSlide(0);
+
+// const nextSlide = function () {
+//   currentSlide == slidesNumber ? (currentSlide = 0) : currentSlide++;
+
+//   moveToSlide(currentSlide);
+//   // activateCurrentDot(currentSlide);
+// };
+// btnRight.addEventListener('click', function (e) {
+//   nextSlide();
+// });
+
+// const previousSlide = function () {
+//   currentSlide === 0 ? (currentSlide = slidesNumber) : currentSlide--;
+
+//   moveToSlide(currentSlide);
+//   // activateCurrentDot(currentSlide);
+// };
+
+// btnLeft.addEventListener('click', function (e) {
+//   previousSlide();
+// });
