013d56669a63fe262a9ea1e0877edf582e691b49
LyoLik1 <lyosha.neznakomov@gmail.com>
2023-08-29 19:32:47 +0300

    create: modal window and validation

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
+const menuButton = document.querySelector('.menu-button');
+const navbarBottom = document.querySelector('.navbar-bottom');
+const bookingButton = document.querySelector('.booking__button');
+const overlay = document.querySelector('.modal__overlay');
+const modalWindow = document.querySelector('.modal__dialog');
+const closeModalButton = document.querySelector('.modal__close');
+const packagesSection = document.querySelector('.packages-wrapper');
+const activitiesSection = document.querySelector('.activities-wrapper');
+const phoneInput = document.querySelectorAll('.input__phone');
+// const phoneValidatorMessage = document.querySelectorAll('.phone__validator');
 const hotelSwiper = new Swiper('.hotel-slider', {
-  // Optional parameters
   loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider-button__next',
     prevEl: '.hotel-slider-button__prev',
   },
-
-  // effect: 'cards',
-  // keyboard: {
-  //   enabled: true,
-  // },
   effect: 'fade',
 });
 
@@ -36,14 +34,99 @@ const reviewsSwiper = new Swiper('.reviews-slider', {
   },
   // effect: 'fade',
 });
+$(document).ready(function () {
+  // Задаем скорость параллакса
+  var parallaxSpeed = 0.3; // Экспериментируйте с этим значением
 
-$('.parallax-window').parallax({
-  imageSrc: '../img/newsletter-bg.jpeg',
-  position: '-400px -400px',
+  // Получаем высоту окна браузера
+  var windowHeight = $(window).height();
+
+  // Получаем высоту изображения
+  var imageHeight = $('.parallax-bg').height();
+
+  // Получаем начальное значение прокрутки (снизу страницы)
+  var initialScroll = imageHeight - windowHeight;
+
+  // При прокрутке страницы
+  $(window).scroll(function () {
+    // Получаем значение прокрутки
+    var scrollTop = initialScroll - $(this).scrollTop();
+
+    // Рассчитываем позицию фона
+    var bgPosition = 'center ' + scrollTop * parallaxSpeed + 'px';
+
+    // Применяем позицию фона к элементу .parallax-bg
+    $('.parallax-bg').css('background-position', bgPosition);
+  });
 });
+// $('.parallax-window').parallax({
+//   imageSrc: '../img/newsletter-bg.jpeg',
+//   position: '-400px -400px',
+// });
 
-const menuButton = document.querySelector('.menu-button');
-const navbarBottom = document.querySelector('.navbar-bottom');
 menuButton.addEventListener('click', function (e) {
   navbarBottom.classList.toggle('navbar-bottom--active');
+});
+
+bookingButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(111);
+  modalWindow.classList.remove('modal__dialog--disable');
+  overlay.classList.remove('modal__overlay--disable');
+});
+// Close Modal Window
+closeModalButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalWindow.classList.add('modal__dialog--disable');
+  overlay.classList.add('modal__overlay--disable');
+});
+overlay.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalWindow.classList.add('modal__dialog--disable');
+  overlay.classList.add('modal__overlay--disable');
+});
+
+document.addEventListener('keydown', function (e) {
+  if (overlay.classList.contains('modal__overlay--disable')) {
+    return;
+  }
+  if (e.key === 'Escape') {
+    modalWindow.classList.add('modal__dialog--disable');
+    overlay.classList.add('modal__overlay--disable');
+  }
+});
+
+packagesSection.addEventListener('click', function (e) {
+  if (e.target.classList.contains('proposition__button')) {
+    modalWindow.classList.remove('modal__dialog--disable');
+    overlay.classList.remove('modal__overlay--disable');
+  } else {
+    return;
+  }
+});
+
+activitiesSection.addEventListener('click', function (e) {
+  if (e.target.classList.contains('card__button')) {
+    modalWindow.classList.remove('modal__dialog--disable');
+    overlay.classList.remove('modal__overlay--disable');
+  } else {
+    return;
+  }
+});
+
+phoneInput.forEach((input) => {
+  const phoneValidatorMessage = input.nextElementSibling; // Получаем следующий элемент после инпута
+
+  input.addEventListener('blur', function (e) {
+    const inputValue = Number(this.value.length);
+    if (inputValue !== 9) {
+      input.style.border = '3px solid red';
+      input.style.marginBottom = '0px';
+      phoneValidatorMessage.classList.remove('phone__validator--disable');
+    } else {
+      phoneValidatorMessage.classList.add('phone__validator--disable');
+      input.style.border = 'none';
+      input.style.marginBottom = '15px';
+    }
+  });
 });
